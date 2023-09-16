df9c476d85e9c3fe67e6bd3486ae25b705c01b04
sailaxmi <sailaxmikomirineni@gmail.com>
2023-09-16 17:07:06 +0000

    solution

---
+++ src/context/CartContext.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const CartContext = React.createContext({
+  cartList: [],
+  removeAllCartItems: () => {},
+  addCartItem: () => {},
+  /*
+  removeCartItem: () => {},
+  
+  incrementCartItemQuantity: () => {},
+  decrementCartItemQuantity: () => {},
+  */
+})
+
+export default CartContext
